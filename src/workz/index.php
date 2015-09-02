3327730a31b303d6081139233474ceca3b7ba034
set_wrong <rothenbaecher.dennis@gmail.com>
2015-09-02 07:45:43 +0000

    init

---
+++ src/workz/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is a part of a new working package.
+ *
+ * (c) Dennis  <rothenbaecher.dennis@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Workz;
+
+require '../../vendor/autoload.php';
+
+var_dump(get_included_files());
